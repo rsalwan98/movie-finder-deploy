b82604639066da3cbf86135ef457ebfa3ad9503d
unknown <rohit.salwan@jungleegames.com>
2020-08-01 21:12:17 +0530

    Updates

---
+++ precache-manifest.95f30d9ca018d6b4c5d1b194489393e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fb779943fdc520c8a5c49b777d8398",
+    "url": "/movie-finder-deploy/index.html"
+  },
+  {
+    "revision": "ec5122982e203ddb7bcc",
+    "url": "/movie-finder-deploy/static/css/main.de5cb643.chunk.css"
+  },
+  {
+    "revision": "ec8bf7ae8a4e7e6e65d3",
+    "url": "/movie-finder-deploy/static/js/2.e7029555.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-finder-deploy/static/js/2.e7029555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec5122982e203ddb7bcc",
+    "url": "/movie-finder-deploy/static/js/main.eee1a369.chunk.js"
+  },
+  {
+    "revision": "b7018505b81a64e3ff9e",
+    "url": "/movie-finder-deploy/static/js/runtime-main.910667c2.js"
+  }
+]);
